1fb153822e1753827a8698b9682fa02c6e2d8d5c
RaihanOprinande <opri.fahdi@gmail.com>
2025-10-15 12:42:22 +0700

    selesai api untuk soal quiz

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\PsychoeducationController;
+use App\Http\Controllers\Api\SoalQuizController;
 use App\Http\Controllers\Api\TemaQuizController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,9 +20,16 @@ Route::delete('TemaQuiz/{id}',[TemaQuizController::class,'destroy']);
 // END API TEMA QUIZ
 
 // API PSYCOEDUCATION
+
 Route::get('psycoedu',[PsychoeducationController::class,'index']);
 Route::get('psycoedu/{id}',[PsychoeducationController::class,'show']);
 
-
-
 // END API PSYCOEDUCATION
+
+// API SOAL QUIZ
+
+Route::get('soal_quiz',[SoalQuizController::class,'index']);
+Route::get('soal_quiz/{id}',[SoalQuizController::class,'show']);
+
+
+// END API SOAL QUIZ
